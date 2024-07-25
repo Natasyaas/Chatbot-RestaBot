5ccf51de9db320a8eda631212f2025479c61df6d
salma natasya <salmanatasya@gmail.com>
2024-07-25 11:19:46 +0700

    first commit

---
+++ RestaBot/app/src/test/java/com/vanesa/RestaBot/MsgModalTest.java
@@ -0,0 +1,7 @@
+package com.vanesa.RestaBot;
+
+import junit.framework.TestCase;
+
+public class MsgModalTest extends TestCase {
+
+}
---
+++ RestaBot/app/src/test/java/com/vanesa/RestaBot/RetroFitApiTest.java
@@ -0,0 +1,7 @@
+package com.vanesa.RestaBot;
+
+import junit.framework.TestCase;
+
+public class RetroFitApiTest extends TestCase {
+
+}
